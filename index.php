550b20e44341d141cc45b6e396a5d657eeb58636
CloudSalander <ruben.as532@gmail.com>
2025-03-13 10:30:02 +0100

    Added even/odd condition and count down function signature

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
     //Lets talk about filter_var too!
     $number = intval(readline());
-    var_dump($number);
+    //Maybe too smart way to say if a number is even :P
+    if($number&1 === 0) printCountDown($number);
+    else printCountDown($number,1);
+
+    function printCountDown(int $number,int $limit = 0): void {
+        var_dump($limit);
+    }
 ?>
