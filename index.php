e8fa1454612b1ed66b642035abb48a3a535675a8
CloudSalander <ruben.as532@gmail.com>
2025-03-13 10:46:50 +0100

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+    //TODO: what about negative numbers?
     //Lets talk about filter_var too!
     $number = intval(readline());
-    if($number % 2 == 0) printCountDown($number);
-    else printCountDown($number,1);
+    if($number % 2 == 0) printCount($number);
+    else printCount($number,1);
 
-    function printCountDown(int $number,int $limit = 0): void {
+    function printCount(int $number,int $limit = 0): void {
         $con = $number;
         while($con > $limit) {
+            //Sure we can refactor that
             if($limit == 0 && $con%2 === 0) echo $con.PHP_EOL;
             else if($limit === 1 && $con&1 === 1) echo $con.PHP_EOL;
             --$con;
